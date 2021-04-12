8435163bf8a2f5f90cefbbda8bed2f6352a9749a
KingDrofd <kingdrofd@gmail.com>
2021-04-12 10:11:35 +0000

    Parallax2D 1.1
    Minor changes(tried to use as few if else statements as possible), and cleaned unnecessary code

--- Parallax/Scripts/Parallax.cs
+++ Parallax/Scripts/Parallax.cs
@@ -12,12 +12,10 @@ public class Parallax : MonoBehaviour
         public float speed;
 
         
-        public SpriteRenderer spriteRenderer;
+       [HideInInspector] public SpriteRenderer spriteRenderer;
         #region       alternations
 
-        public bool left = false, down = false;
-
-
+        
         public bool alternate = false;
         [@Range(2, 10, 2/*This can be any number depending how you will alternate and how many*/)]
         public int alternation; //Minimum is 2,
@@ -27,44 +25,44 @@ public class Parallax : MonoBehaviour
         #endregion 
     }
 
-    
+    public enum Type
+    {
+        Left, Down
+    }
+
+
+    public Type type;
     public SpriteParallax spriteParallax;
+
     private void Start()
     {
-        if (transform.childCount > spriteParallax.alternation && spriteParallax.alternate == true)
-        {           
             CheckChildren(); 
-        }
-        else if (transform.childCount < spriteParallax.alternation && spriteParallax.alternate == true)
-        {
-            ChildrenCheck();
-        }
+        
             spriteParallax.spriteRenderer = GetComponent<SpriteRenderer>();
             spriteParallax.startPos = spriteParallax.spriteRenderer.transform.position;
             spriteParallax.itemSize = spriteParallax.spriteRenderer.bounds.size;
-}
+    }
 
     #region errors
     void CheckChildren()
     {
-       
-            Debug.LogError("Incorrect Number Of Children Or Alternation On " + gameObject + ". Need:" + (transform.childCount - spriteParallax.alternation) + " Less " +
+       if(transform.childCount > spriteParallax.alternation && spriteParallax.alternate == true)
+       {
+                Debug.LogError("Incorrect Number Of Children Or Alternation On " + gameObject + ". Need:" + (transform.childCount - spriteParallax.alternation) + " Less " +
                " Children," + " Or " + (transform.childCount - spriteParallax.alternation) + " More " +
                " Alternation");
-        
-   }
-    void ChildrenCheck()
-    {
-
-        Debug.LogError("Incorrect Number Of Children Or Alternation On " + gameObject + ". Need:" + (transform.childCount - spriteParallax.alternation) * (-1) + " More " +
+       }
+       else if (transform.childCount < spriteParallax.alternation && spriteParallax.alternate == true)
+       {
+            Debug.LogError("Incorrect Number Of Children Or Alternation On " + gameObject + ". Need:" + (transform.childCount - spriteParallax.alternation) * (-1) + " More " +
            " Children," + " Or " + (transform.childCount - spriteParallax.alternation) * (-1) + " Less " +
            " Alternation");
-
+       }
     }
+    
     #endregion
     private void ParallaxMoveX()
-    {
-    
+    {   
         if (spriteParallax.alternate == true)
         {
             float newPos = Mathf.Repeat(Time.time * spriteParallax.speed, spriteParallax.itemSize.x * spriteParallax.alternation);
@@ -77,42 +75,35 @@ public class Parallax : MonoBehaviour
         }
     }
     private void ParallaxMoveY()
-    {
-
-
+    {       
         if (spriteParallax.alternate == true)
         {
             float newPos = Mathf.Repeat(Time.time * spriteParallax.speed, spriteParallax.itemSize.y * spriteParallax.alternation);
             transform.position = spriteParallax.startPos + Vector3.down * newPos;
-        }
-
-        else 
+        }else 
         {
             float newPos = Mathf.Repeat(Time.time * spriteParallax.speed, spriteParallax.itemSize.y);
             transform.position = spriteParallax.startPos + Vector3.down * newPos;
         }
-        
-
     }
     private void Initialize()
-    {
-        if (spriteParallax.left == true && spriteParallax.down == true)
+    {      
+        switch (type)
         {
-            Debug.LogError("Can't Have Two Parallax Dections At The Same Time On " + gameObject);
-        }
-        else
-        {
-            if (spriteParallax.left == true)
-                ParallaxMoveX();
-
-            if (spriteParallax.down == true)
-                ParallaxMoveY();
+            case Type.Down:
+                {
+                    ParallaxMoveY();
+                    break;
+                }
+            case Type.Left:
+                {
+                    ParallaxMoveX();
+                    break;
+                }
         }
     }
     private void FixedUpdate()
     {
         Initialize();
     }
-
-    
 }
